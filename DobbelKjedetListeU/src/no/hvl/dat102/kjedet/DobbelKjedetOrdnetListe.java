d57afb3aafc6414dd59496bae7a24a149a1bb4fa
h600884 <89258811+h600884@users.noreply.github.com>
2022-02-21 13:40:19 +0100

    Add files via upload

---
+++ DobbelKjedetListeU/src/no/hvl/dat102/kjedet/DobbelKjedetOrdnetListe.java
@@ -0,0 +1,160 @@
+package no.hvl.dat102.kjedet;
+
+import no.hvl.dat102.adt.DobbelKjedetOrdnetListeADT;
+import no.hvl.dat102.kjedet.DobbelNode;
+import no.hvl.dat102.exceptions.EmptyCollectionException;
+
+public class DobbelKjedetOrdnetListe<T extends Comparable<T>> implements DobbelKjedetOrdnetListeADT<T> {
+	private DobbelNode<T> foerste;
+	private DobbelNode<T> midten;
+	private DobbelNode<T> siste;
+	private int antall;
+
+	public DobbelKjedetOrdnetListe(T minVerdi, T maksVerdi) {
+		
+		DobbelNode<T> nyNode1 = new DobbelNode<T>(minVerdi);
+		foerste = nyNode1;
+		midten = foerste;
+
+		DobbelNode<T> nyNode2 = new DobbelNode<T>();
+		nyNode2.setElement(maksVerdi);
+		nyNode1.setNeste(nyNode2);
+		nyNode2.setForrige(nyNode1);
+		siste = nyNode2;
+
+		antall = 0;
+	
+		// Kjeding
+		//TODO
+	}
+
+	@Override
+	public void leggTil(T el) {
+		DobbelNode<T> nyNode = new DobbelNode<T>(el);
+		DobbelNode<T> aktuell = foerste.getNeste();
+		while ((el.compareTo(aktuell.getElement()) > 0)) {
+			aktuell = aktuell.getNeste();
+		}
+		// Legg inn foran aktuell
+		nyNode.setNeste(aktuell);
+		nyNode.setForrige(aktuell.getForrige());
+		aktuell.getForrige().setNeste(nyNode);
+		aktuell.setForrige(nyNode);
+		antall++;
+
+	}
+
+	@Override
+	public T fjern(T el) {
+		T resultat = null;
+		if (erTom())
+			throw new EmptyCollectionException("dobbelkjedet ordnet liste er tom");
+		DobbelNode<T> aktuell = finn(el);
+		if (aktuell != null) {// returner og slett
+			resultat = aktuell.getElement();
+			aktuell.getForrige().setNeste(aktuell.getNeste());
+			aktuell.getNeste().setForrige(aktuell.getForrige());
+
+		}
+
+		return resultat;
+
+	}
+
+	/*
+	 * Returnerer referansen til noden hvis el fins, ellers returneres
+	 * null-referansen
+	 */
+	private DobbelNode<T> finn(T el) {
+		DobbelNode<T> node = null;
+		DobbelNode<T> p = null;
+
+	
+		if (el.compareTo(midten.getElement()) >= 0) { 
+			p = midten; 
+		} else { 
+			p = foerste.getNeste();
+		}
+		while (el.compareTo(p.getElement()) > 0) {
+			p = p.getNeste();
+		} 
+		
+		if (el.compareTo(p.getElement()) == 0) {
+			node = p;
+		}
+		return node;
+
+	}
+	
+	@Override
+	public boolean fins(T el) {
+		boolean funnet = false;
+		DobbelNode<T> p = null;
+		if ((el.compareTo(foerste.getElement()) <= 0) || (el.compareTo(siste.getElement()) >= 0)) {
+			System.out.println("Ugyldig verdi. verdi > " + foerste.getElement() + "verdi < " + siste.getElement());
+
+		} else { 
+			if (el.compareTo(midten.getElement()) >= 0) { 
+				p = midten; 
+			} else { 
+				p = foerste.getNeste();
+			}
+			while (el.compareTo(p.getElement()) > 0) {
+				p = p.getNeste();
+			} 
+			if (el.compareTo(p.getElement()) == 0) {
+				funnet = true;
+			}
+		} 
+		return funnet;
+	}
+	
+	@Override
+	public void visListe() {
+		DobbelNode<T> p = foerste;
+		
+		while(p != null) {
+			System.out.println(p.getElement());
+			p = p.getNeste();
+		}
+		System.out.println("Første: " + foerste.getElement() + "Midten: " + 
+		midten.getElement() + "Siste: " + siste.getElement());
+		System.out.println();
+	}
+	
+	@Override
+	public boolean erTom() {
+		return (antall == 0);
+	}
+
+	@Override
+	public int antall() {
+		return antall;
+	}
+
+	public String toString() {
+		String resultat = "";
+		DobbelNode<T> aktuell = foerste.getNeste();
+		while (aktuell != siste) {
+
+			resultat = resultat + aktuell.getElement().toString();
+			aktuell = aktuell.getNeste();
+		}
+
+		return resultat;
+	}
+
+	public String tilStrengBaklengs() {
+		String resultat = "";
+		DobbelNode<T> aktuell = siste.getForrige();
+		while (aktuell != foerste) {
+
+			resultat = resultat + aktuell.getElement().toString();
+			aktuell = aktuell.getForrige();
+		}
+
+		return resultat;
+
+	}
+
+}
