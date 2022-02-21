eaeb64407849d9ea8f09e3b7505a85b9d06b7bdd
h600884 <89258811+h600884@users.noreply.github.com>
2022-02-21 13:38:30 +0100

    Add files via upload

---
+++ ListeOrdnetU/src/no/hvl/dat102/listeklient/Person.java
@@ -0,0 +1,65 @@
+package no.hvl.dat102.listeklient;
+
+public class Person implements Comparable<Person> {
+
+	private String fornavn;
+	private String etternavn;
+	private int foedselsaar;
+
+	public Person() {
+		this("", "", 0);
+
+	}
+
+	public Person(String fornavn, String etternavn, int foedselsaar) {
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.foedselsaar = foedselsaar;
+
+	}
+
+	public void setFoedselsaar(int foedselsaar) {
+		this.foedselsaar = foedselsaar;
+	}
+
+	public void setEtternavn(String etternavn) {
+		this.etternavn = etternavn;
+	}
+
+	public void setFornavn(String fornavn) {
+		this.fornavn = fornavn;
+	}
+
+	int getFoedselsaar() {
+		return foedselsaar;
+	}
+
+	public String getEtternavn() {
+		return etternavn;
+	}
+
+	public String getFornavn() {
+		return fornavn;
+	}
+
+	public String toString() {
+		return (foedselsaar + "\t" + etternavn + ", " + fornavn);
+	}
+
+	public int compareTo(Person denAndrePersonen) {
+		
+		int resultat = 0;
+		if(foedselsaar < denAndrePersonen.getFoedselsaar()) {
+			resultat = -1;
+		} else if (this.foedselsaar == denAndrePersonen.getFoedselsaar()) {
+			if(this.etternavn != denAndrePersonen.getEtternavn() || this.fornavn != denAndrePersonen.getFornavn()) {
+					resultat = 0;
+				
+			}
+		} else  {
+			resultat = 1;
+		}
+		return resultat;
+	}//
+
+}// class
---
+++ ListeOrdnetU/src/no/hvl/dat102/tabell/TabellOrdnetListe.java
@@ -0,0 +1,149 @@
+package no.hvl.dat102.tabell;
+
+import no.hvl.dat102.adt.OrdnetListeADT;
+import no.hvl.dat102.exceptions.EmptyCollectionException;
+
+public class TabellOrdnetListe<T extends Comparable<T>> implements OrdnetListeADT<T> {
+
+	private final static int STDK = 100;
+	private final static int IKKE_FUNNET = -1;
+	private int ant;
+	private T[] liste;
+
+	public TabellOrdnetListe() {
+		this(STDK);
+	}
+
+	public TabellOrdnetListe(int startKapasitet) {
+		ant = 0;
+		liste = (T[]) (new Comparable[startKapasitet]);
+	}
+
+	@Override
+	public T fjernSiste() {
+		if (erTom())
+			throw new EmptyCollectionException("ordnet liste");
+
+		T resultat = siste();
+		ant--;
+		liste[ant] = null;
+		return resultat;
+	}
+
+	@Override
+	public T fjernFoerste() {
+		if (erTom())
+			throw new EmptyCollectionException("ordnet liste");
+
+		T resultat = foerste();
+		ant--;
+		for(int i = 0; i < ant; i++) {
+			liste[i] = liste[i+1];
+		}
+		liste[ant] = null;
+		
+		return resultat;
+	}
+
+	@Override
+	public T foerste() {
+		if (erTom())
+			throw new EmptyCollectionException("ordnet liste");
+
+		T resultat = liste[0];
+		return resultat;
+	}
+
+	@Override
+	public T siste() {
+		if (erTom())
+			throw new EmptyCollectionException("ordnet liste");
+	
+		return liste[ant - 1];
+	}
+
+	@Override
+	public boolean erTom() {
+		return (ant == 0);
+	}
+
+	@Override
+	public int antall() {
+		return ant;
+	}
+
+	@Override
+	public void leggTil(T element) {
+		if(ant == liste.length) {
+		utvid();
+		}
+		if(erTom()) {
+			liste[0] = element;
+		} else {
+			boolean funnet = false;
+			for(int i = 0; i < ant && !funnet; i++) {
+				if(element.compareTo(liste[i]) >= 0) {
+					for(int j = ant - 1; j >= 1; j--) {
+						liste[j + 1] = liste[j];
+					}
+					liste[i] = element;
+					funnet = true;
+				}
+			}
+		}
+		ant++;
+	}
+
+	@Override
+	public boolean inneholder(T element) {
+		return (finn(element) != IKKE_FUNNET);
+	}
+
+	@Override
+	public T fjern(T element) {
+		int i = finn(element);
+
+		if (i != -1) {
+			for (int j = i; j < ant - 1; j++) {
+				liste[j] = liste[j + 1];
+			}
+			ant--;
+			liste[ant] = null;
+			return element;
+		}
+		return null;
+
+	}
+
+	private int finn(T el) {
+		if (erTom()) {
+			throw new EmptyCollectionException("Ordnet liste");
+		}
+		for (int i = 0; i < ant; i++) {
+			if (liste[i].equals(el)) {
+				return i;
+			}
+		}
+		return IKKE_FUNNET;
+	}
+
+	public String toString() {
+		String resultat = "";
+
+		for (int i = 0; i < ant; i++) {
+			resultat = resultat + liste[i].toString() + "\n";
+		}
+		return resultat;
+	}
+
+	private void utvid() {
+		T[] hjelpeTabell = (T[]) (new Comparable[liste.length * 2]);
+
+		for (int i = 0; i < liste.length; i++) {
+			hjelpeTabell[i] = liste[i];
+		}
+
+		liste = hjelpeTabell;
+	}
+
+}// class
